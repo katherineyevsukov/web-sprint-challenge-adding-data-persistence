c39e1a89161625a58992cdaba1fde3f7088592f5
Kat <kyevsukov@gmail.com>
2021-12-10 10:02:59 -0500

    can get all resources

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,2 +1,9 @@
 // build your `Resource` model here
 const db = require("./../../data/dbConfig")
+
+async function getAll(){
+    const resources = await db('resources')
+    return resources
+}
+
+module.exports = { getAll }
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,4 +1,13 @@
 // build your `/api/resources` router here
 const router = require('express').Router()
+const Resource = require('./model')
 
+router.get('/', async (req, res, next) => {
+    try {
+        const resources = await Resource.getAll()
+        res.json(resources)
+    } catch(err){
+        next(err)
+    }
+})
 module.exports = router
