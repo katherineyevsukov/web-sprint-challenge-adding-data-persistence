165da39fbedda3f804d3e4e116943f920f882423
Kat <kyevsukov@gmail.com>
2021-12-10 14:55:58 -0500

    fixed task middleware

--- api/task/middleware.js
+++ api/task/middleware.js
@@ -4,7 +4,7 @@ async function validateTaskBody(req, res, next) {
   if (!req.body.task_description || !req.body.task_description.trim()) {
     next({ status: 400, message: "task description is required" });
   } else if (!req.body.project_id) {
-    next({ status: 400, message: "task description is required" });
+    next({ status: 400, message: "project id is required" });
   } else {
     next();
   }
