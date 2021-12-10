2b69e4094a58c498bd6a9f8967121564f1f1ed20
Kat <kyevsukov@gmail.com>
2021-12-10 09:41:25 -0500

    second seed file

--- data/seeds/02-resources.js
+++ data/seeds/02-resources.js
@@ -1,13 +1,9 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+  return knex('projects').insert([   
+    { resource_name: "Tailwind CSS", resource_description: "style using using a library"},
+    { resource_name: "Global State Management", resource_description: "Redux vs. Hooks"},
+    { resource_name: "RDBMS", resource_description: "relational database management sytems"},
+    { resource_name: "JavaScript: The Definitive Guide", resource_description: "classes, promises, generators, & more"},
+    { resource_name: "SQL", resource_description: "how to query a database"},
+  ]);
 };
