aa86dba806a85fbe55ebcdb1c19e72acb359ce91
Kat <kyevsukov@gmail.com>
2021-12-10 08:51:34 -0500

    server running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res, next) => {
+    res.send('server is up and running!')
+})
+
+server.use((err, req, res, next) => {
+    res.status(err. status|| 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = server
