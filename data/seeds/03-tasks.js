a2778cf49792eb386358b66095550033ae939fb3
Kat <kyevsukov@gmail.com>
2021-12-10 09:51:45 -0500

    updated seeds

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   return knex('tasks').insert([   
-    { task_description: "Write Endpoints", task_notes: "don't forget to reformat data", task_completed: false, project_id: 2},
-    { task_description: "Host on Heroku", task_notes: "make sure to add env variables", task_completed: false, project_id: 2},
+    { task_description: "Write Endpoints", task_notes: "don't forget to reformat data", task_completed: true, project_id: 2},
+    { task_description: "Host on Heroku", task_notes: "make sure to add env variables", task_completed: true, project_id: 2},
     { task_description: "Host on Vercel", task_notes: "create a Vercel account", task_completed: true, project_id: 1},
   ]);
 };
